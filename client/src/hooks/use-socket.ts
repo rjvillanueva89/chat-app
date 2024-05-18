df976d1273cf3566508b11c0e7e73834cffbb692
RJ Villanueva <rj.villanueva89@gmail.com>
2024-05-19 00:03:42 +0800

    adjust env for client

--- client/src/hooks/use-socket.ts
+++ client/src/hooks/use-socket.ts
@@ -1,13 +1,15 @@
 import { useEffect } from "react"
 import { io } from "socket.io-client"
 
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL ?? "http://localhost:3000"
+
 interface Props<T> {
   channel: string
   onPayload: (payload: T) => void
 }
 
 export const useSocket = <T>({ channel, onPayload }: Props<T>) => {
-  const socket = io("http://localhost:3000")
+  const socket = io(SOCKET_URL)
   const emit = (payload: T) => {
     socket.emit("message", { channel, payload })
   }
